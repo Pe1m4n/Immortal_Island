cb435718a96aaa358e7ba75da6c589846f7702c5
g.khokhlov <g.khokhlov@corp.nekki.ru>
2020-06-13 14:12:36 +0300

    SkeletonKiller component

---
+++ Assets/Source/Fight/World/SkeletonKiller.cs
@@ -0,0 +1,18 @@
+﻿using Source.Fight.Enemies;
+using UnityEngine;
+
+namespace Source.Fight.World
+{
+    [RequireComponent(typeof(Collider))]
+    public class SkeletonKiller : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            var enemy = other.gameObject.GetComponentInParent<Enemy>();
+            if (enemy != null)
+            {
+                Destroy(enemy.gameObject);
+            }
+        }
+    }
+}
