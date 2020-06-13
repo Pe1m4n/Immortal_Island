310e7c0f51a4f26c825bae7870d9c661308c50f2
g.khokhlov <g.khokhlov@corp.nekki.ru>
2020-06-13 21:53:41 +0300

    Cheat

---
+++ Assets/Source/Fight/Cheat.cs
@@ -0,0 +1,39 @@
+﻿using Source.Fight.World;
+using UnityEngine;
+using Zenject;
+
+namespace Source.Fight
+{
+    public class Cheat : ITickable
+    {
+        private readonly WinLoseController _winLoseController;
+        private float _nextClickCombo;
+        private int _comboClicks;
+
+        public Cheat(WinLoseController winLoseController)
+        {
+            _winLoseController = winLoseController;
+        }
+
+        public void Tick()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                if (Time.time < _nextClickCombo)
+                {
+                    _comboClicks++;
+                }
+                else
+                {
+                    _comboClicks = 1;
+                }
+
+                _nextClickCombo = Time.time + 0.3f;
+                if (_comboClicks >= 5)
+                {
+                    _winLoseController.Win();
+                }
+            }
+        }
+    }
+}
