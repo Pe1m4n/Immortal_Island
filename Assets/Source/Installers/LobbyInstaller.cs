d1ac57d30f42f07264d57dba6bba9bc55279eac1
g.khokhlov <g.khokhlov@corp.nekki.ru>
2020-06-13 15:39:56 +0300

    Lobby -> Fight transition

---
+++ Assets/Source/Installers/LobbyInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Source.Installers
+{
+    public class LobbyInstaller : MonoInstaller
+    {
+        [SerializeField] private LocationLoaderController _locationLoader;
+        public override void InstallBindings()
+        {
+            Container.Inject(_locationLoader);
+        }
+    }
+}
---
+++ Assets/Source/Installers/LocationsInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.Installers
+{
+    [CreateAssetMenu(fileName = "LocationsInfo", menuName = "Immortal_Island/Locations info", order = 0)]
+    public class LocationsInfo : ScriptableObject
+    {
+        [SerializeField] private List<string> _locationNames;
+
+        public List<string> LocationNames => _locationNames;
+    }
+}
