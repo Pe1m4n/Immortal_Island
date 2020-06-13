ea77a2810f746e74c31ac7477ffc4ab32ba324ae
g.khokhlov <g.khokhlov@corp.nekki.ru>
2020-06-13 14:34:50 +0300

    Spawn and destroy components

---
+++ Assets/Source/Common/DestroyMeComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Source.Common
+{
+    public class DestroyMeComponent : MonoBehaviour
+    {
+        [SerializeField] private float _destroyAfterSeconds;
+
+        private float _currentTime;
+        
+        private void Update()
+        {
+            if (_currentTime >= _destroyAfterSeconds)
+            {
+                Destroy(gameObject);
+            }
+
+            _currentTime += Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Source/Common/SpawnerComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Source.Common
+{
+    public class SpawnerComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameObject; 
+
+        public void Spawn()
+        {
+            if (_gameObject == null)
+            {
+                return;
+            }
+
+            Instantiate(_gameObject);
+        }
+    }
+}
